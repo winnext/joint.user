4ef822c7a5c3ff76d12dfe7b934bae89b84a6a30
sgnm-mihriceren <mihri.ceren@signumtte.com>
2021-10-06 10:46:39 +0300

    Forgot password -confirm password service

--- src/__tests__/register/register.test.ts
+++ src/__tests__/register/register.test.ts
@@ -11,3 +11,9 @@ test('Register function', async () => {
   expect(res).not.toBeInstanceOf(Error);
   cognito.deleteUser('testuser', 'us-east-1_CzF6pu0Sv');
 });
+test('Forgot password function', async () => {
+  const cognito = new CognitoService();
+  const res = await cognito.forgotPassword('sgnm1049')
+  expect(res).not.toBeInstanceOf(Error);
+  
+});
